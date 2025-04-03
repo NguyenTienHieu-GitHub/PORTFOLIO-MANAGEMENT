1ead469251f5ed6f400df219a1d5ec7c28ad1e81
NguyenTienHieu-GitHub <you@example.com>
2025-04-04 02:51:14 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      {" "}
-      {/* Chỉ bao bọc tại đây */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+reportWebVitals();
